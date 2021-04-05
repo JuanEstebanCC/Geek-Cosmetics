5b192a99f2d3f48dcd2301eb129abc88f5584a9a
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-04-05 18:36:20 -0500

    Connection and get data wiht Typescript done

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,9 +1,10 @@
-import { mysql } from 'mysql2';
 
-export const connection = mysql.createConnection({
+import {createConnection, QueryError, RowDataPacket} from 'mysql2';
+export const connection = createConnection({
   host: 'localhost',
   user: 'root',
   database: 'geek_cosmetics',
   password: 'LCELMIELV'
 });
+
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, {Application} from 'express';
 require('dotenv').config({ path: __dirname+'/.env' });
 const app: Application = express();
+import { QueryError, RowDataPacket} from 'mysql2';
+import {connection} from './database/db';
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -8,7 +10,11 @@ app.use(express.urlencoded({extended:false}));
 app.set('port',4300)
 
 app.get('/', (req: express.Request,res: express.Response) => {
-  res.send("Hello word")
+  connection.query('SELECT * FROM  orders', (err: QueryError, rows: RowDataPacket[]) => {
+    console.log('The solution is: ', rows);
+  res.send("Hello word"+rows)
+});
+
 })
 app.listen(app.get('port'), () => {
   console.log(`Server running on port ${app.get('port')}`)
